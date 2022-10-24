ce94604c1280b2bf608a7ba015e9d599fe776adf
Calhoon <BRIAN.CALHOON@tetratech.com>
2022-10-24 15:40:37 -0400

    BC

--- Day 1 - points script.R
+++ Day 1 - points script.R
@@ -6,7 +6,18 @@ library(tidyverse)
 library(sf)
 library(rcartocolor)
 library(ggfx)
+library(terra)
+library(maptiles)
+library(extrafont)
+library(extrafontdb)
+library(showtext)
 
+#Load some fonts----
+
+#from Google
+
+
+font_add_google(name = "Amatic SC", family = "amatic-sc")
 #read in bikeshare data. 
 #accessed here: https://s3.amazonaws.com/capitalbikeshare-data/index.html
 df <- read_csv("bikeshare data sept 2022.csv")
@@ -18,6 +29,7 @@ df <- df %>%
          , !is.na(end_lng)
          , !is.na(start_lat)
          , !is.na(start_lng)) 
+
 
 #select one day to work with - 9/23/2022
 df <- df %>% 
@@ -43,8 +55,18 @@ df_group<- df1 %>%
 df_group2 <- df2 %>% 
   group_by(end_station_id, end_station_name) %>% 
   count()
+
 #read in the administrative boundaries
 dc <- st_read("DC/Single_Member_District_from_2023.shp")
+
+#get dc maptile from maptiles package (requires terra)
+
+tile <- get_tiles(dc
+                  , provider = "CartoDB.DarkMatterNoLabels"
+                  , crop = TRUE)
+
+plot(tile)
+
 
 #create a bbox to use in coord_sf below.
 bbox = st_bbox(dc)
@@ -152,3 +174,193 @@ p_even
 library(patchwork)
 
 p_morn + p_even
+
+
+###Testing out the monthly morning commute avg., Sept. 2022
+
+#read in bikeshare data. 
+#accessed here: https://s3.amazonaws.com/capitalbikeshare-data/index.html
+df <- read_csv("bikeshare data sept 2022.csv")
+
+
+
+df <- df %>% 
+  filter(!is.na(start_lat)
+         , !is.na(start_lng)) 
+
+df$started_at <- df$started_at %>% 
+  mdy_hm()
+
+#create columns for time and day
+df <- df %>% 
+  mutate(time = format(started_at, format = "%H:%M:%S")
+         , day = format(started_at, format = "%Y-%m-%d"))
+
+#create a geometry column out of te lng and lat columns
+df_sf <- df %>% 
+  st_as_sf(coords = c("start_lng", "start_lat")
+           ,crs = 4326)
+
+#filter for just morning rides
+df_morn <- df_sf %>% 
+  filter(time >= "06:00:00" & time <= "09:00:00")
+
+#group by bike station and avg. rides starting there in the morning
+df_morn2 <- df_morn %>% 
+  select(ride_id
+         , start_station_name
+         , start_station_id
+         , time
+         , day
+         , geometry) %>%
+  filter(!is.na(start_station_name)) %>% 
+  group_by(day, start_station_name, start_station_id) %>%
+  summarize(count = n()) %>% 
+  ungroup() %>% 
+  group_by(start_station_name) %>% 
+  summarize(avg = mean(count)) %>% 
+  slice_head(n = 100) #taking only the top 40
+
+#Then, repeat for the end rides but have to delete missing lng and lat
+df_sf_end <- df %>% 
+  filter(!is.na(end_lng)
+         , !is.na(end_lat)) %>% 
+  st_as_sf(coords = c("end_lng", "end_lat")
+           ,crs = 4326)
+
+df_morn_end <- df_sf_end %>% 
+  filter(time >= "06:00:00" & time <= "09:00:00")
+
+df_morn_end2 <- df_morn_end %>% 
+  select(ride_id
+         , end_station_name
+         , end_station_id
+         , time
+         , day
+         , geometry) %>%
+  filter(!is.na(end_station_name)) %>% 
+  group_by(day, end_station_name, end_station_id) %>%
+  summarize(count = n()) %>% 
+  ungroup() %>% 
+  group_by(end_station_name) %>% 
+  summarize(avg = mean(count)) %>% 
+  slice_head(n = 100)
+
+
+p <- ggplot()+
+  geom_sf(data = dc, fill = "black", color = "white"
+          , size = .25) +
+  with_outer_glow(geom_sf(data = df_morn2, aes(geometry = st_jitter(geometry))
+                          , color = "yellow"
+                          , alpha = .6)) +
+  with_outer_glow(geom_sf(data = df_morn_end2, aes(geometry = st_jitter(geometry))
+                          , col = "red"
+                          , alpha = .2)) +
+  coord_sf(xlim = c(bbox[[1]], bbox[[3]])
+           , ylim = c(bbox[[2]], bbox[[4]])) +
+  theme_void() +
+  #labs(subtitle = "Mornings: \n6 am - 9 am") +
+  theme(plot.subtitle = element_text(size = 20
+                                     , family = "amatic-sc")
+        )
+
+p_plus <- p + annotate(geom = "text"
+                         , x = bbox[[1]]+.015
+                         , y = bbox[[4]]-.02
+                         , label = "Mornings"
+                         , family = "amatic-sc"
+                         , size = 16)
+
+
+#Evening rides in Sept-----
+
+#create a geometry column out of te lng and lat columns
+df_sf <- df %>% 
+  st_as_sf(coords = c("start_lng", "start_lat")
+           ,crs = 4326)
+
+#filter for just morning rides
+df_even <- df_sf %>% 
+  filter(time >= "16:00:00" & time <= "19:00:00")
+
+#group by bike station and avg. rides starting there in the morning
+df_even2 <- df_even %>% 
+  select(ride_id
+         , start_station_name
+         , start_station_id
+         , time
+         , day
+         , geometry) %>%
+  filter(!is.na(start_station_name)) %>% 
+  group_by(day, start_station_name, start_station_id) %>%
+  summarize(count = n()) %>% 
+  ungroup() %>% 
+  group_by(start_station_name) %>% 
+  summarize(avg = mean(count)) %>% 
+  slice_head(n = 100) #taking only the top 40
+
+#Then, repeat for the end rides but have to delete missing lng and lat
+df_sf_end <- df %>% 
+  filter(!is.na(end_lng)
+         , !is.na(end_lat)) %>% 
+  st_as_sf(coords = c("end_lng", "end_lat")
+           ,crs = 4326)
+
+df_even_end <- df_sf_end %>% 
+  filter(time >= "16:00:00" & time <= "19:00:00")
+
+df_even_end2 <- df_even_end %>% 
+  select(ride_id
+         , end_station_name
+         , end_station_id
+         , time
+         , day
+         , geometry) %>%
+  filter(!is.na(end_station_name)) %>% 
+  group_by(day, end_station_name, end_station_id) %>%
+  summarize(count = n()) %>% 
+  ungroup() %>% 
+  group_by(end_station_name) %>% 
+  summarize(avg = mean(count)) %>% 
+  slice_head(n = 100)
+
+#plot the evening cycle stations
+
+showtext_auto()
+p1 <- ggplot()+
+  geom_sf(data = dc, fill = "black", color = "white"
+          , size = .25) +
+  with_outer_glow(geom_sf(data = df_even2, aes(geometry = st_jitter(geometry))
+                          , color = "yellow"
+                          , alpha = .6)) +
+  with_outer_glow(geom_sf(data = df_even_end2, aes(geometry = st_jitter(geometry))
+                          , col = "red"
+                          , alpha = .2)) +
+  coord_sf(xlim = c(bbox[[1]], bbox[[3]])
+           , ylim = c(bbox[[2]], bbox[[4]])) +
+  #annotate(geom = "text", aes(x = bbox[[1]], y = bbox[[4]]), label = "Evenings: \n4 pm - 7 pm")+
+  theme_void() +
+  #labs(subtitle = "Evenings) + 
+  theme(plot.subtitle = element_text(size = 24
+                                     , family = "amatic-sc"))
+p1_plus <- p1 + annotate(geom = "text"
+             , x = bbox[[1]]+.015
+             , y = bbox[[4]]-.02
+             , label = "Evenings"
+             , family = "amatic-sc"
+             , size = 16)
+patch <- p_plus + p1_plus # n = 100
+
+showtext_auto()
+patch + plot_annotation(
+  title = "Day 1: DC's top 100 Capital Bikeshare stations, September 2022"
+  , caption = "Data: Capital Bikeshare, https://s3.amazonaws.com/capitalbikeshare-data/index.html \nAuthor: @BCalhoon7 \n #30DayMapChallenge"
+) & theme(plot.title = element_text(size = 44
+                                    , face = "bold"
+                                    , family = "amatic-sc")
+          , plot.title.position = "panel"
+          , plot.caption = element_text(size = 16
+                                        , family = "amatic-sc")
+)
+#testing out with some additional size
+
