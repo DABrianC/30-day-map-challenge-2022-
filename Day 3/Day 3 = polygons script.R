1ec6b2dd5d7349788f316187c016237475b9ccef
Calhoon <BRIAN.CALHOON@tetratech.com>
2022-11-02 11:02:51 -0400

    BC

--- Day 3/Day 3 = polygons script.R
+++ Day 3/Day 3 = polygons script.R
@@ -4,7 +4,7 @@
 
 library(tidyverse)
 library(sf)
-library(rcartocolor)
+library(MetBrewer)
 library(ggfx)
 library(lubridate)
 library(ggtext)
@@ -20,7 +20,71 @@ set.seed(11012022)
 
 font_add_google(name = "Amatic SC", family = "amatic-sc")
 
+
+
+#The above plot is ok, but this plot is cleaner
+
+lanes <- st_read("Day 3/Bicycle_Lanes.shp")
+
+
+#join the bike lanes to ANC polygons 
+#and count how many are in each ANC
+join <- dc |> st_join(lanes) |>
+  group_by(ANC_ID.x) |>
+  count() 
+
+hok <- met.brewer(name = "Hokusai2"
+                  , type = "continuous")
+
+showtext_auto()
+p <- ggplot() +
+  geom_sf(data = join, aes(geometry = geometry
+                    , fill = n)
+          , color = "white"
+          , size = 0) +
+  geom_sf(data = lanes, alpha = .3, size = .5)+
+  theme_void()+
+  scale_fill_gradientn(name = "# of bike lanes\nin each ANC"
+    , colors=met.brewer("Hokusai2")
+    , breaks = c(25, 250)
+    , labels = c("Less", "More")) +
+  labs(title = "Does your DC neighborhood have enough bike lanes?"
+       , subtitle = "Official bike lanes in grey"
+       , caption = "Data: Open Data DC\nVisualized by: @bcalhoon7 #30DayMapChallenge") +
+  theme(plot.title.position = "plot"
+        , plot.title = element_text(family = "amatic-sc"
+                                    , size = 40
+                                    , face = "bold"
+                                    , color = hok[[6]])
+        , plot.subtitle = element_text(family = "amatic-sc"
+                                       , size = 34
+                                       , face = "bold"
+                                       , color = "grey")
+        , plot.caption = element_text(family = "amatic-sc"
+                                      , size = 24
+                                      , face = "bold"
+                                      , color = hok[[6]]
+                                      , hjust = .5)
+        , legend.text = element_text(family = "amatic-sc"
+                                     , size = 24
+                                     , color = hok[[6]])
+        , legend.title = element_text(family = "amatic-sc"
+                                      , size = 28
+                                      , face = "bold"
+                                      , color = hok[[6]])
+  )
+  
+
+ggsave(plot = p
+       , filename = "Day 3/cycle routes by ANC.png"
+       , height = 4
+       , width = 4
+       , unit = "in"
+       , bg = "white")
+
+###Discarded this plot - it works, but it didn't say much-----
 carto_colors <- rcartocolor::carto_pal(7, "SunsetDark")
+
 
 
 #read in bikeshare data. 
@@ -74,29 +138,32 @@ dest <- df |>
 dest_anc <- dc |> st_join(dest)
 
 #Plot the two maps
-ggplot() +
+porig <- ggplot() +
   geom_sf(data = orig_anc, aes(geometry = geometry
-          , fill = n)
-          , color = "white"
+                               , fill = log(n))
+          , color = "grey"
           , size = 0) +
-  theme_void() +
-  scale_fill_viridis_c(option = "inferno"
-                       , direction = -1)
+  theme_void()
 
-
-scale_fill_carto_c(name = ""
-                   , type = "continuous"
-                   , palette = "Purp"
-                   , direction =1)
-
-
-ggplot() +
+pdest <- ggplot() +
   geom_sf(data = dest_anc, aes(geometry = geometry
-                               , fill = n)
-          , color = "white"
+                               , fill = log(n))
+          , color = "grey"
           , size = 0) +
-  theme_void() +
-  scale_fill_carto_c(name = ""
+  theme_void() 
+
+#combine the two plots
+plots <- porig + pdest
+
+plots + plot_annotation(
+  title = "Great title"
+  , subtitle = "informative subtitle"
+  , caption = "Data: Open Data DC, Capital Bikeshare\n
+  Visualized by: @bcalhoon7 #30DayMapChallenge") +
+  plot_layout(guides = "collect") &
+  scale_fill_carto_c(limits = range(c(log(orig_anc$n), log(dest_anc$n))
+                                    , na.rm = T)
+                     , name = "# of bikes hired \nand returned"
                      , type = "continuous"
-                     , palette = "PurpOr"
-                     , direction =1)
+                     , palette = "Sunset"
+                     , direction = -1)
