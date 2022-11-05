26d1a75af15d383a642523b8a7a390bb987519ec
Calhoon <BRIAN.CALHOON@tetratech.com>
2022-11-05 12:23:00 -0400

    BC

--- Day 5 - Ukraine.R
+++ Day 5 - Ukraine.R
@@ -50,28 +50,14 @@ colors <- c(rev(c1[2:4]), yellows)
 
 rgl::rgl.close()
 mat %>%
-  # This adds the coloring, we're passing in our `colors` object
   height_shade(texture = grDevices::colorRampPalette(colors)(256)) %>%
   plot_3d(heightmap = mat, 
-          # This is my preference, I don't love the `solid` in most cases
           solid = FALSE, 
-          # You might need to hone this in depending on the data resolution;
-          # lower values exaggerate the height
           z = 12,
-          # Set the location of the shadow, i.e. where the floor is.
-          # This is on the same scale as your data, so call `zelev` to see the
-          # min/max, and set it however far below min as you like.
           shadowdepth = 50,
-          # Set the window size relatively small with the dimensions of our data.
-          # Don't make this too big because it will just take longer to build,
-          # and we're going to resize with `render_highquality()` below.
           windowsize = c(800*wr,800*hr), 
-          # This is the azimuth, like the angle of the sun.
-          # 90 degrees is directly above, 0 degrees is a profile view.
           phi = 90, 
           zoom = 1, 
-          # `theta` is the rotations of the map. Keeping it at 0 will preserve
-          # the standard (i.e. north is up) orientation of a plot
           theta = 0, 
           background = "white") 
 
@@ -79,7 +65,6 @@ mat %>%
 render_camera(phi = 10, zoom = .7, theta = 120)
 
 ###---render high quality
-# Ensure dir exists for these graphics
 if (!dir.exists(glue("Day 5/{map}"))) {
   dir.create(glue("Day 5/{map}"))
 }
@@ -96,35 +81,19 @@ saveRDS(list(
   outfile = outfile
 ), "Day 5/hoverla/header.rds")
 
-# Wrap this in brackets so it runs as chunk
 {
-  # Test write a PNG to ensure the file path is good.
-  # You don't want `render_highquality()` to fail after it's 
-  # taken hours to render.
   png::writePNG(matrix(1), outfile)
-  # I like to track when I start the render
   start_time <- Sys.time()
   cat(glue("Start Time: {start_time}"), "\n")
   render_highquality(
-    # We test-wrote to this file above, so we know it's good
-    outfile, 
-    # See rayrender::render_scene for more info, but best
-    # sample method ('sobol') works best with values over 256
+      outfile, 
     parallel = TRUE,
     samples = 300, 
-    # Turn light off because we're using environment_light
     light = FALSE, 
-    # All it takes is accidentally interacting with a render that takes
-    # hours in total to decide you NEVER want it interactive
     interactive = FALSE,
-    # HDR lighting used to light the scene
     environment_light = "./Day 5/phalzer_forest_01_4k.hdr",
-    # Adjust this value to brighten or darken lighting
     intensity_env = 1.75,
-    # Rotate the light -- positive values move it counter-clockwise
     rotate_env = 90,
-    # This effectively sets the resolution of the final graphic,
-    # because you increase the number of pixels here.
     width = round(6000 * wr), height = round(6000 * hr)
   )
   end_time <- Sys.time()
@@ -137,27 +106,18 @@ img <- image_read("./Day 5/hoverla/hoverla_ukraine_z10.png")
 #set text color
 text_color <- colors[[3]]
 
-#fonts
-library(showtext)
-library(extrafont)
-
-font_import(paths = "C:/USERS/BRIAN.CALHOON/APPDATA/LOCAL/MICROSOFT/WINDOWS/FONTS")
-load_showtext_fonts()
-font_add_google(name = "Fredericka the Great", family = "frederika")
-
-font_add(family = "frederika"
-         , regular = "C:/USERS/BRIAN.CALHOON/APPDATA/LOCAL/MICROSOFT/WINDOWS/FONTS/FREDERICKATHEGREAT-REGULAR.ttf")
-loadfonts()
-showtext_auto()
-img_ <- image_annotate(img, "Ukraine's Highest Point"
-                       , font = "Castellar"
+#set the font
+font <- "Copperplate Gothic"
+{
+img_ <- image_annotate(img, "Day 5: Ukraine's Highest Point"
+                       , font = font
                        , color = text_color
                        , size = 125
                        , gravity = "north"
                        , location = "+0+200")
 
 img_ <- image_annotate(img_, "Mount Hoverla", weight = 700
-                       , font = "Castellar"
+                       , font = font
                        , location = "+0+400"
                        , color = text_color
                        , size = 200
@@ -166,7 +126,7 @@ img_ <- image_annotate(img_, "Mount Hoverla", weight = 700
 elevation <- max(mat, na.rm = T)
 
 img_ <- image_annotate(img_, glue("Elevation: {scales::label_comma()(elevation)} m")
-                       , font = "Castellar" 
+                       , font = font 
                        , location = "+1200-1300"
                        , color = text_color
                        , size = 110
@@ -203,26 +163,11 @@ img_comp <- image_composite(img_, inset
 # Caption
 img_comp2 <- image_annotate(img_comp, glue("Visualized by @DABrianC | #30DayMapChallenge", 
                                   " | Made with : #Rayshader & #Elevatr") 
-                       , font = "Castellar"
+                       , font = font
                        , location = "+0+50"
                        , color = text_color
                        , size = 75
                        , gravity = "south")
 
-# Twitter
-#twitter <- fontawesome::fa("twitter", fill = text_color, fill_opacity = .5)
-#grid::grid.newpage()
-
-#tmp <- tempfile()
-#png(tmp, bg = "transparent")
-#grid::grid.draw(svgtools::read_svg(twitter))
-#dev.off()
-
-#tw <- image_read(tmp)
-#tw <- image_scale(tw, "x75")
-
-#img_ <- image_composite(img_, tw, gravity = "south",
-#                        offset = "-530+65")
-
-
 image_write(img_comp2, glue("Day 5/hoverla/hoverla_fully_annotated.png"))
+}
